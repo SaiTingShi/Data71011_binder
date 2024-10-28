c8abee17b9d6841e1f7000ac27c8f5afcf7a2d02
SaiTingShi <88271563+SaiTingShi@users.noreply.github.com>
2024-10-29 01:56:36 +0800

    Update python_file_test.py

--- python_file_test.py
+++ python_file_test.py
@@ -1,1 +1,7 @@
+import pandas as pd
 print('Hello World!')
+data1 = [[1,2,3],
+         [4,5,6],
+         [3,6,8]]
+table1 = pd.DataFrame(data1,columns = ['column1','column2','column3'], index = ['row1','row2',''row3])
+print(table1)
